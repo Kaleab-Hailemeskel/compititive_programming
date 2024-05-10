0c237e763ab71a2cead93ba6c7455d73e63d2f19
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-05-10 22:11:07 +0300

    Create Number of Smaller.py

---
+++ python_language/Number of Smaller.py
@@ -0,0 +1,8 @@
+size1, size2 = map(int, input().split(" "))
+arr1 = list(map(int, input().split(" ")))
+arr2 = list(map(int, input().split(" ")))
+index1 = 0
+for index2 in range(size2):
+    while index1 < size1 and arr1[index1] < arr2[index2]:
+        index1 += 1
+    print(index1, end = " ")
