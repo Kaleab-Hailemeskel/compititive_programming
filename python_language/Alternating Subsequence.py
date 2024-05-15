e69395e8c7d9ea727d8898803caf755bd64b5fc9
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-05-15 22:44:50 +0300

    Update Alternating Subsequence.py

--- python_language/Alternating Subsequence.py
+++ python_language/Alternating Subsequence.py
@@ -2,8 +2,6 @@ itt = int(input())
 for i in range(itt):
     size = int(input())
     arr = list(map(int, input().split(" ")))
-    pos = True
-    LOW = -10 ** 9
     maxInTheSet = arr[0]
     aSum = 0
     for i in range(1, size):
