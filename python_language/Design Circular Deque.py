048829adeaa7b86717d2a1a4af4b105c3d0a9910
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-05-21 00:43:07 +0300

    Create  Design Circular Deque.py

---
+++ python_language/Design Circular Deque.py
@@ -0,0 +1,82 @@
+class Deque:
+    def __init__(self, val :int):
+        self.val = val
+        self.next = None
+        self.previous = None
+
+class MyCircularDeque:
+
+    def __init__(self, val: int):
+        self.maxSize = val
+        self.currSize = 0
+        self.head = None
+        self.tail = None
+
+    def insertFront(self, value: int) -> bool:
+        if self.currSize >= self.maxSize:
+            return False
+        newNode = Deque(value)
+        if not self.head:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            newNode.next = self.head
+            self.head.previous = newNode
+            self.head = newNode
+        self.currSize += 1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.currSize >= self.maxSize:
+            return False
+        newNode = Deque(value)
+        if not self.head:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            newNode.previous = self.tail
+            self.tail = newNode
+        self.currSize += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        if not self.currSize:
+            return False
+        if self.currSize  == 1:
+            self.head = self.tail = None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            if self.head:
+                self.head.prevoius = None
+            del temp
+        self.currSize -= 1
+        return True
+    
+    def deleteLast(self) -> bool:
+        if not self.currSize:
+            return False
+        if self.currSize == 1:
+            self.head = self.tail = None
+        else:
+            temp = self.tail
+            self.tail = self.tail.previous
+            if self.tail:
+                self.tail.next = None
+        
+            del temp
+        self.currSize -= 1
+        return True
+
+    def getFront(self) -> int:
+        return self.head.val if self.currSize else -1
+           
+    def getRear(self) -> int:
+        return self.tail.val if self.currSize else - 1
+
+    def isEmpty(self) -> bool:
+        return False if self.currSize else True
+
+    def isFull(self) -> bool:
+        return True if self.currSize == self.maxSize else False
