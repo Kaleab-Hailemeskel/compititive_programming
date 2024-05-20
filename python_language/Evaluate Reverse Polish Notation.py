6ce8873f47abae055c884df2fc94b410429c7a7c
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-05-20 23:04:40 +0300

    Create Evaluate Reverse Polish Notation.py

---
+++ python_language/Evaluate Reverse Polish Notation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        ope = {"*":'*', "/":"/", "+":"+", "-":"-"}    
+        num = []
+        for val in tokens:
+            if val in ope:
+                l = num.pop()
+                f = num.pop()
+                if val == "+":
+                    num.append(l + f)
+                elif val == "-":
+                    num.append(f - l)
+                elif val == "/":
+                    f = math.ceil(f/l) if f/l < 0 else math.floor(f/l)
+                    num.append(f)
+                else:
+                    ans = f * l
+                    num.append(ans)
+            else:
+                num.append(int(val))
+
+        return num[0]
