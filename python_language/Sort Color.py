c15811e2838f396113afb9eabcc4f790363f815b
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-07-18 19:07:09 +0300

    Create Sort Color.py

---
+++ python_language/Sort Color.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = [0, 0, 0]
+        n = len(nums)
+        for i in range(n):
+            count[nums[i]] += 1
+        curr_start = 0
+        for i in range(3):
+            for j in range(curr_start, curr_start + count[i]):
+                nums[j] = i
+            curr_start += count[i]
+            
+        
+                
