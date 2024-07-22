f547f4b29e22d45a295a42b2056783742887e379
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-07-22 23:07:03 +0300

    Update Number of Smaller.py

--- python_language/Number of Smaller.py
+++ python_language/Number of Smaller.py
@@ -6,3 +6,18 @@ for index2 in range(size2):
     while index1 < size1 and arr1[index1] < arr2[index2]:
         index1 += 1
     print(index1, end = " ")
+
+# after some time
+
+n, m = map(int, input().split())
+arr_1 = list(map(int, input().split()))
+arr_2 = list(map(int, input().split()))
+ans = [0] * m
+i, k = 0, 0
+for j in range(m):
+    while i < n and arr_1[i] < arr_2[j]:
+        i += 1
+    ans[j] = i
+print(* ans)
+
+    
