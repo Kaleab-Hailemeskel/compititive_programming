32845ddc29fcec6663770676db48cc4cee5dc786
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-07-30 14:28:33 +0300

    Update Min Stack.py
    Updated to The correct solution

--- python_language/Min Stack.py
+++ python_language/Min Stack.py
@@ -2,18 +2,23 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min_val = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        if not self.min_val or self.min_val[-1] >= val:
+            self.min_val.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
+        val = self.stack.pop()
+        if self.min_val and val == self.min_val[-1]:
+            self.min_val.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.min_val[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
