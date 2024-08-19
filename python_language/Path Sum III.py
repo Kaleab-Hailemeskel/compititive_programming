02741cd8de7673536a3ed3accce640947dfbdab9
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-08-19 20:04:30 +0300

    Create Path Sum III.py

---
+++ python_language/Path Sum III.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        count_path = 0
+        def dfs(node, sum_):
+            if not node: 
+                return 0
+            sum_ += node.val
+            return int(sum_ == targetSum) + dfs(node.right, sum_) + dfs(node.left, sum_)
+            
+        
+        def left_right(node):
+            if not node: return 0
+            return dfs(node, 0) + left_right(node.left) + left_right(node.right)
+        
+        
+        return left_right(root)
