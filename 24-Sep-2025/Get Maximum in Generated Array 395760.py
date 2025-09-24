6a599fe34f16820dc22e49d6b52718b4cb7a723c
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2025-09-24 18:26:47 +0300

    Created 'Get Maximum in Generated Array' by A2SV Hub

---
+++ 24-Sep-2025/Get Maximum in Generated Array 395760.py
@@ -0,0 +1,14 @@
+# Problem: Get Maximum in Generated Array - https://leetcode.com/problems/get-maximum-in-generated-array/description/
+
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        if n == 0:
+            return 0
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        for i in range(2, n + 1):
+            if i % 2:
+                dp[i] = dp[i // 2] + dp[i //2 + 1]
+            else:
+                dp[i] = dp[i // 2]
+        return max(dp)
