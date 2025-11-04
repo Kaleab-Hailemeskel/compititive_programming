e0c66b45f0fdc9071bcd8de81755261d37dc2265
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2025-11-04 13:37:00 +0300

    Created 'Selection of Personnel' by A2SV Hub

---
+++ 04-Nov-2025/Selection of Personnel 407249.py
@@ -0,0 +1,22 @@
+# Problem: Selection of Personnel - https://codeforces.com/problemset/problem/630/F
+
+import sys, math
+from random import randint
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heapify, heappop
+from collections import defaultdict, Counter, deque
+rand_value = randint(1, 100000)
+def rand(x):
+    return x ^ rand_value
+
+def sol():	
+    n = int(sys.stdin.readline().strip())
+    return sum( math.comb(n, m) for m in range(5, 8))
+    
+    
+
+
+itt = 1
+# itt = int(int(sys.stdin.readline().strip()))
+for _ in range(itt):
+    print(sol())
