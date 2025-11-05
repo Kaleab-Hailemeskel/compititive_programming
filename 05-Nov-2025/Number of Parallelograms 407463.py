9c9b1273c1ef39ed73b10b7b656ac5b4f200faa8
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2025-11-05 11:37:37 +0300

    Created 'Number of Parallelograms' by A2SV Hub

---
+++ 05-Nov-2025/Number of Parallelograms 407463.py
@@ -0,0 +1,45 @@
+# Problem: Number of Parallelograms - https://codeforces.com/contest/660/problem/D
+
+import sys, math
+from random import randint
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heapify, heappop
+from collections import defaultdict, Counter, deque
+rand_value = randint(1, 100000)
+def rand(x):
+    return x ^ rand_value
+
+def sol():	
+    n = int(sys.stdin.readline().strip())
+    mid_bisections = Counter()
+    points = []
+    for _ in range(n):
+        x, y = map(int, sys.stdin.readline().strip().split())                
+        points.append((x, y))
+    
+    for i in range(n - 1):
+        x, y = points[i]
+        for j in range(i + 1, n):
+            old_x, old_y = points[j]
+            mid_x = x + old_x
+            mid_y = y + old_y
+            
+            mid_bisections[(mid_x, mid_y)] += 1
+            
+
+    
+    return sum(math.comb(count, 2) for count in mid_bisections.values())
+        
+        
+    
+    
+                
+                
+    pass # Remove
+    
+
+
+itt = 1
+# itt = int(int(sys.stdin.readline().strip()))
+for _ in range(itt):
+    print(sol())
